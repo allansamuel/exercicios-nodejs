adf2a1fa6cfde8945511241453470002b6ed69d1
allansamuel <allangomeseu@hotmail.com>
2019-11-29 17:01:15 -0300

    Model definition

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,41 @@
-const mysql = require('mysql')
-const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-})
+'use strict'
+
+const Sequelize = require('sequelize')
+      bcrypt = require('bcrypt-nodejs')
+
+const config = require('../config'),
+      db = require('../services/database')
+
+
+const modelDefinition = {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}
+    
+const modelOptions = {
+    instanceMethods: {
+        comparePasswords: comparePasswords
+    },
+    hooks: {
+        beforeValidate: hashPassword
+    }
+}
+
+const UserModel = db.define('user', modelDefinition, modelOptions);
+
+function comparePasswords(password, callback){
+
+}
+
+function hashPassword(password, callback){
+
+}
+
+module.exports = UserModel
